fcac4c3122d36b768c176d63311d768ad7cc0bba
the-weird-aquarian <github.serenify@simplelogin.fr>
2023-11-06 21:06:15 +0530

    updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("com.nulab-inc:zxcvbn:1.8.2")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 }
